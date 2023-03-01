c0bd86b3d235821b8e3d5a16ddf25564adb2bc0d
Marianne Agulhon <marianne.agulhon@gmail.com>
2023-03-01 16:38:15 +0100

    Do Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import foodList from './foods.json';
+import FoodBox from './components/FoodBox/FoodBox';
 
 function App() {
+  const [foods, setFoods] = useState(foodList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Food List</h2>
+      {foods.map((food) => {
+        /* return (
+          <div key={food.name}>
+            <p>{food.name}</p>
+            <img src={food.image} width={100} alt="" />
+          </div>
+        );
+      })} */
+
+        //Iteration 3 | Render a List of FoodBox Components
+        return <FoodBox key={food.name} food={food} />;
+      })}
     </div>
   );
 }
+
+// function Example() {
+//   // After importing the components we can render them directly:
+//   return (
+//     <div>
+//       <Row>
+//         <Col>
+//           <Divider>Fancy Input</Divider>
+//           <Input value={""} onChange={() => {}} />
+//         </Col>
+
+//         <Col>
+//           <Card title={"Fancy Card"}>
+//             <Button onClick={() => {}}>Fancy Button</Button>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </div>
+//   );
+// }
 
 export default App;
